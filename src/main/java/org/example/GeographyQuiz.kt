74b21306b4e02d14e5eff252071866f13130265c
Douglas Jasper <doug.jasper@trios.com>
2025-11-18 09:26:06 -0500

    Arrays Created

--- src/main/java/org/example/GeographyQuiz.kt
+++ src/main/java/org/example/GeographyQuiz.kt
@@ -1,5 +1,25 @@
 package org.example
 
+import kotlin.random.Random
+
 fun main() {
-    println("Welcome to the Geography Quiz!")
+
+    val provinces = arrayOf("Alberta", "British Columbia", "Manitoba",
+        "New Brunswick", "Newfoundland & Labrador", "Northwest Territories",
+        "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island",
+        "Quebec", "Saskatchewan", "Yukon")
+
+    val capitals = arrayOf("Edmonton", "Victoria", "Winnipeg", "Fredericton",
+        "St. John's", "Yellowknife", "Halifax", "Iqaluit", "Toronto",
+        "Charlottetown", "Quebec City", "Regina", "Whitehorse")
+
+    var duplicates =arrayOf(false, false, false, false,
+        false, false, false, false,
+        false, false, false, false,
+        false)
+
+    var answer = ""
+    var correct = 0
+
+    println("Welcome to the Provinces Geography Quiz - Good Luck!")
 }
